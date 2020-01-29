9d8f66fa99d697a17fefc01c98cc1fb082791533
Henry Quinn <henryquinniv@gmail.com>
2020-01-28 21:37:28 -0500

    Polishing contact form.

--- src/components/application.js
+++ src/components/application.js
@@ -1,36 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default () => (
   <div>
-    <p id="scroll-contact">&nbsp;</p>
+    <p id="scroll-application">&nbsp;</p>
     <table width="600" border="1" cellspacing="1" cellpadding="4">
       <tr>
         <td width="88">
-          <p>Contact</p>
+          <p >Application</p>
         </td>
         <td width="487"> 
-          <form name="contact" method="POST" data-netlify="true">
-            <p>
-              <label>Your Name: <input type="text" name="name" /></label>   
-            </p>
-            <p>
-              <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-              <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select></label>
-            </p>
-            <p>
-              <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        
+          <h3>Henry Quinn Tech Enthusiast Scholarship</h3>
+          <p>Through the support and encouragement of some teachers here, I spent my high school years taking independent studies and continuing adult education classes in order to learn how to build websites and fix computers. After graduating college and landing a fun tech job, I want to pay it forward.</p>
+          <p>This is for the new group of students messing with computers, setting up game servers for their friends, exploring networks they come across, and learning through experimentation.</p>
+          <p>This scholarship awards $500 to two East Hampton High School seniors who intend to major (or otherwise continue their education in) in computer science or a related field of study. Please return your application to the Guidance Office by <strong>March 27th, 2020.</strong></p>
+          <u>Please attach a word-processed response with the following information:</u>
+          <ol>
+            <li>Name</li>
+            <li>Where do you plan on going to school? (College name, city and state)</li>
+            <li>Short Questions - Just a few sentences for each:
+              <p>What do you want to major in?</p>
+              <p>Any ideas what you want to do for a career yet?</p>
+              <p>Were there any life experiences, interests, or activities that got you interested in working with technology?</p>
+            </li>
+            <li>Short Essays - No more than 300 words for each:
+
+              <p>In lieu of there not being a computer science program at EHHS, describe a project you’ve worked on to learn (or teach yourself) more about technology? [My personal favorite was figuring out how to hypothetically hide a folder full of networked games on the school network so my graduating class had something to do during study halls!]</p>
+            
+            <p>Describe a piece of new technology (whether it be an app, a robot, or some kind of gadget) that you want to help create someday.</p>
+            </li>
+          </ol>
         </td>
       </tr>
     </table>
