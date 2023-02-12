d6f31c9c4e7f2827668d33e3a11507cbe87fd988
Henry Quinn <Quinncuatro@users.noreply.github.com>
2023-02-12 15:46:37 -0500

    Updating due date.

--- src/components/application.js
+++ src/components/application.js
@@ -14,7 +14,7 @@ export default () => (
           <p>Through the support and encouragement of some teachers here, I spent my high school years taking independent studies and continuing adult education classes in order to learn how to build websites and fix computers. After graduating college and landing a fun tech job, I want to pay it forward.</p>
           <p>This is for the new group of students messing with computers, setting up game servers for their friends, exploring networks they come across, and learning through experimentation.</p>
           <p>This scholarship awards $500 to two East Hampton High School seniors who intend to major (or otherwise continue their education) in computer science or a related field of study.</p>
-          <p>Please complete the form below, or submit a word-processed version of your responses to the Guidance Office, by <strong><u>May 7th, 2021.</u></strong></p>
+          <p>Please complete the form below, or submit a word-processed version of your responses to the Guidance Office, by <strong><u>Monday April 17th, 2023.</u></strong></p>
           <ApplyForm />
         </td>
       </tr>
